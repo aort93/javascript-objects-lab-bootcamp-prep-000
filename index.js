81c9a7d1234e99a51718831f42826d3c119117b6
Alonso Ortega <alonsojortega@gmail.com>
2017-08-25 04:35:49 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,3 +9,17 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
 
   return object
 }
+
+function deleteFromObjectByKey(object, key) {
+  const newObj = Object.assign({}, object)
+
+  delete newObj[key]
+
+  return newObj
+}
+
+function destructivelyDeleteFromObjectByKey(object, key) {
+  delete object[key]
+
+  return object
+}
